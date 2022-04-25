94faf621c006cf18dbf97e3adefd270b84c9dac3
duongtuyen2002 <kubjnpro80@gmail.com>
2022-04-25 07:53:05 +0700

    first commit

---
+++ app/src/main/java/com/example/myrecyclerview/MyAdapter.java
@@ -0,0 +1,80 @@
+package com.example.myrecyclerview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.zip.Inflater;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String data1[],data2[];
+    int images [];
+    Context context;
+
+    public MyAdapter(Context context, String[] data1, String[] data2, int[] images) {
+        this.data1 = data1;
+        this.data2 = data2;
+        this.images = images;
+        this.context = context;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row,parent,false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImage.setImageResource(images[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,SecondActivity.class);
+                intent.putExtra("data1",data1[position]);
+                intent.putExtra("data2",data2[position]);
+                intent.putExtra("myImage",images[position]);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1,myText2;
+        ImageView myImage;
+        ConstraintLayout mainLayout;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.myText1);
+            myText2 = itemView.findViewById(R.id.myText2);
+            myImage = itemView.findViewById(R.id.myImage);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myrecyclerview/SecondActivity.java
@@ -0,0 +1,46 @@
+package com.example.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ImageView mainImageView;
+    TextView title, description;
+
+    String data1,data2;
+    int myImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        mainImageView = findViewById(R.id.mainImageView);
+        title = findViewById(R.id.tittle);
+        description = findViewById(R.id.description);
+
+        getData();
+        setData();
+    }
+
+    public void getData(){
+        if(getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")){
+
+            data1 = getIntent().getStringExtra("data1");
+            data2 = getIntent().getStringExtra("data2");
+            myImage = getIntent().getIntExtra("myImage",1);
+        }else{
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void setData(){
+        title.setText(data1);
+        description.setText(data2);
+        mainImageView.setImageResource(myImage);
+    }
+}
